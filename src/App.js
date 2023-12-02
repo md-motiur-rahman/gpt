1a06e221455468a727be6abe17534e6588824f59
md-motiur-rahman <cse.motiur@gmail.com>
2023-12-02 00:12:18 +0000

    gpt 3 full project

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+import { Header, Footer, Blog, Features, Possibility, WhatGPT3 } from "./containers";
+import { Cta, Navbar, Brand } from "./components";
+import "./App.css";
+function App() {
+  return (
+    <div className="App">
+      <div className="gradient__bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <Cta />
+      <Blog />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/brand/import.js
@@ -0,0 +1,13 @@
+import google from '../../assets/google.png';
+import dropbox from '../../assets/dropbox.png';
+import slack from '../../assets/slack.png';
+import shopify from '../../assets/shopify.png';
+import artissan from '../../assets/atlassian.png';
+
+export{
+    google,
+    dropbox,
+    slack,
+    shopify,
+    artissan
+}
